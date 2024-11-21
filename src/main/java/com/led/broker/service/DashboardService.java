2d05d06cae2e35739f8d65812e61d3d7c4fc4c26
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-21 15:53:50 -0300

    Criando compose

--- src/main/java/com/led/broker/service/DashboardService.java
+++ src/main/java/com/led/broker/service/DashboardService.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(value = "dashboard", url = "http://192.168.10.1:8081/totem")
+@FeignClient(value = "dashboard", url = "http://localhost:8081/totem")
 public interface DashboardService {
     @GetMapping("/dashboard/gerar")
     public DashboardResponse atualizarDashboard(@RequestHeader("Authorization") String authorization);
