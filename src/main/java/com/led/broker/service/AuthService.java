e3759c23796cf002d28f00e804394bd7bf9398b9
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-21 16:35:04 -0300

    Criando compose

--- src/main/java/com/led/broker/service/AuthService.java
+++ src/main/java/com/led/broker/service/AuthService.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "totem", url = "http://vps55601.publiccloud.com.br:8080/totem")
+@FeignClient(value = "totem", url = "http://vps55601.publiccloud.com.br:8000/totem")
 public interface AuthService {
 
     @GetMapping("/auth/valid")
