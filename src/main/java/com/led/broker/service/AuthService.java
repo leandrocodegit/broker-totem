75726e474729563dc698366fc90be39686a0df9b
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-22 09:29:07 -0300

    Criando compose

--- src/main/java/com/led/broker/service/AuthService.java
+++ src/main/java/com/led/broker/service/AuthService.java
@@ -1,40 +1,16 @@
 package com.led.broker.service;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Service
-public class AuthService {
+@FeignClient(value = "totem", url = "http://vps55601.publiccloud.com.br:8000/totem")
+public interface AuthService {
 
-
-    private final RestTemplate restTemplate;
-    private final String url = "http://vps55601.publiccloud.com.br:8000/totem";
-
-    public AuthService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-
-    public boolean validarToken(String token) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + token);
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<Boolean> response = restTemplate.exchange(url + "/auth/valid", HttpMethod.GET, entity, Boolean.class);
-        return response.getBody();
-    }
-
-    public boolean validarwebSocker(String token) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + token);
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<Boolean> response = restTemplate.exchange(url + "/auth/ws", HttpMethod.GET, entity, Boolean.class);
-        return response.getBody();
-    }
+    @GetMapping("/auth/valid")
+    public void validarToken(@RequestHeader("Authorization") String authorization);
+    @GetMapping("/auth/ws")
+    public void validarwebSocker(@RequestHeader("Authorization") String authorization);
 
 }
