579eae8037e15f5f18ca07b9406c86a1e079a80b
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-23 20:43:02 -0300

    Criando compose

--- src/main/java/com/led/broker/config/FeignConfig.java
+++ src/main/java/com/led/broker/config/FeignConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Configuration
 public class FeignConfig {
@@ -11,5 +12,10 @@ public class FeignConfig {
     @Bean
     public Decoder feignDecoder(HttpMessageConverters httpMessageConverters) {
         return new SpringDecoder(() -> httpMessageConverters);
+    }
+
+    @Bean
+    public HttpMessageConverters httpMessageConverters() {
+        return new HttpMessageConverters(new MappingJackson2HttpMessageConverter());
     }
 }
