7803e6e94fde60956eafb28a0b3d843cf1df9ded
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-28 17:28:55 -0300

    Criado servico conexao

--- src/main/java/com/led/broker/controller/response/DispositivoDashResponse.java
+++ src/main/java/com/led/broker/controller/response/DispositivoDashResponse.java
@@ -1,5 +1,6 @@
 package com.led.broker.controller.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.led.broker.model.Configuracao;
 import com.led.broker.model.Temporizador;
 import com.led.broker.model.constantes.Comando;
@@ -31,6 +32,7 @@ public class DispositivoDashResponse {
     private String enderecoComplento;
     private String enderecoCompleto;
     private boolean isTimer;
+    @JsonIgnore
     private Temporizador temporizador;
     public boolean isTimer() {
         return TimeUtil.isTime(this);
