14e77d9ac78e61e7dd67c1089c8b91112cc7d61a
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-28 09:19:37 -0300

    Criado servico comando

--- src/main/java/com/led/broker/handler/MqttMessageHandler.java
+++ src/main/java/com/led/broker/handler/MqttMessageHandler.java
@@ -28,43 +28,5 @@ public class MqttMessageHandler implements MessageHandler {
     @Override
     @ServiceActivator(inputChannel = "mqttInputChannel")
     public void handleMessage(Message<?> message) {
-        UUID clientId = (UUID) message.getHeaders().get("id");
-        String topico = (String) message.getHeaders().get("mqtt_receivedTopic");
-
-        byte[] bytess = (byte[]) message.getPayload();
-        Object payloads = null;
-        try {
-            payloads = objectMapper.readValue(bytess, Object.class);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        if (topico.startsWith(Topico.DEVICE_SEND)) {
-
-
-            try {
-                byte[] bytes = (byte[]) message.getPayload();
-                Mensagem payload = objectMapper.readValue(bytes, Mensagem.class);
-                payload.setBrockerId(clientId.toString());
-                dispositivoService.atualizarDispositivo(payload);
-
-                if(payload.getComando().equals(Comando.ACEITO)){
-                    if(ComandoService.streams.containsKey(payload.getId())){
-                        ComandoService.streams.remove(payload.getId()).success(payload.getComando().value() + " " + payload.getId());
-                    }
-                }
-
-            } catch (Exception erro) {
-                System.out.println("Erro ao capturar id");
-                erro.printStackTrace();
-            }
-        }
-
-        System.out.println("Mensagem recebida do cliente " + clientId + ": " + message.getPayload().toString());
-    }
-
-    // Método para obter informações de um cliente
-    public String getClientInfo(String clientId) {
-        return clientMap.get(clientId);
     }
 }
--- src/main/java/com/led/broker/service/MqttService.java
+++ src/main/java/com/led/broker/service/MqttService.java
@@ -16,17 +16,6 @@ public class MqttService {
 
     private final MessageHandler mqttOutbound;
 
-    public void sendRetainedMessage(String topic, String message, boolean reter) {
-        message = message.replaceAll("#", "");
-        Message<String> mqttMessage = MessageBuilder.withPayload(message)
-                .setHeader(MqttHeaders.TOPIC, topic)
-                .setHeader(MqttHeaders.RETAINED, true)
-                .build();
-
-        System.out.println("Comando enviado para: " + message);
-        mqttOutbound.handleMessage(mqttMessage);
-    }
-
     public void sendRetainedMessage(String topic, ComandoRequest comandoRequest) {
 
        String message = new Gson().toJson(comandoRequest);
@@ -37,26 +26,5 @@ public class MqttService {
 
         System.out.println("Comando enviado para: " + message);
         mqttOutbound.handleMessage(mqttMessage);
-    }
-
-    public void sendDashboardMessage(DashboardResponse dashboardResponse) {
-
-        String message = new Gson().toJson(dashboardResponse);
-        Message<String> mqttMessage = MessageBuilder.withPayload(message)
-                .setHeader(MqttHeaders.TOPIC, "dashboard")
-                .setHeader(MqttHeaders.RETAINED, true)
-                .build();
-
-        System.out.println("Atualizando dashboard: " + message);
-        mqttOutbound.handleMessage(mqttMessage);
-    }
-
-    public void removeRetainedMessage(String topic) {
-        Message<String> emptyMessage = MessageBuilder.withPayload("") // Mensagem vazia
-                .setHeader(MqttHeaders.TOPIC, topic)
-                .setHeader(MqttHeaders.RETAINED, true) // Define a mensagem como retida
-                .build();
-
-        mqttOutbound.handleMessage(emptyMessage);
     }
 }
