214471e2a5e20a6d5d17bd24589d85254db36b9b
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-28 10:06:18 -0300

    Criado servico comando

--- src/main/java/com/led/broker/model/constantes/Topico.java
+++ src/main/java/com/led/broker/model/constantes/Topico.java
@@ -2,5 +2,6 @@ package com.led.broker.model.constantes;
 
 public class Topico {
 
-   public static String DEVICE_CONFIRMACAO = "device/confirmacao/#";
+    public static String DEVICE_CONFIRMACAO = "device/confirmacao/#";
+    public static String DEVICE_RECEIVE = "device/receive/";
 }
