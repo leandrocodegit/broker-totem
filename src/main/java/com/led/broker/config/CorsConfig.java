b155242032ebcf0880432ffae0a8ac5cf15f8442
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-10 01:08:08 -0300

    Configurando ssl

--- src/main/java/com/led/broker/config/CorsConfig.java
+++ src/main/java/com/led/broker/config/CorsConfig.java
@@ -6,19 +6,24 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.Collections;
+
 @Configuration
 public class CorsConfig {
 
     @Bean
     public CorsWebFilter  corsConfigurationSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOrigin("");
-        corsConfig.addAllowedHeader("*");
-        corsConfig.addAllowedMethod("*");
-        corsConfig.setAllowCredentials(false);
+        corsConfig.addAllowedHeader("*"); // Permitir todos os cabeçalhos
+        corsConfig.addAllowedMethod("*"); // Permitir todos os métodos
+        corsConfig.setAllowCredentials(false); // Não permitir credenciais (cookies, headers de autorização, etc.)
+
+        // Não adicionar nenhuma origem
+        corsConfig.setAllowedOrigins(Collections.emptyList()); // Nenhuma origem permitida
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
+        source.registerCorsConfiguration("/**", corsConfig); // Aplicar a configuração para todos os endpoints
+
         return new CorsWebFilter(source);
     }
 }
