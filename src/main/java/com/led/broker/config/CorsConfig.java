37d4ded925134d9598880287f7e0bb51c69734c1
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-10 01:48:48 -0300

    Configurando ssl

--- src/main/java/com/led/broker/config/CorsConfig.java
+++ src/main/java/com/led/broker/config/CorsConfig.java
@@ -5,20 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+@Configuration
+public class CorsConfig implements WebFluxConfigurer {
 
-public class CorsConfig {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
 
-    @Bean
-    public CorsWebFilter  corsConfigurationSource() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOrigin("http://sincroled.com.br");
-        corsConfig.addAllowedHeader("*");
-        corsConfig.addAllowedMethod("*");
-        corsConfig.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-        return new CorsWebFilter(source);
+        registry.addMapping("/**")
+                .allowedOrigins("http://sincroled.com.br")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
