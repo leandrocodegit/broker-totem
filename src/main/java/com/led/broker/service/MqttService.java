04c53f7f2d8e8961684a9fa5f243f80b6cd6d219
leandrocodegit <lpoliveira.ti@gmail.com>
2025-02-02 15:41:45 -0300

    Correcao agenda

--- src/main/java/com/led/broker/service/MqttService.java
+++ src/main/java/com/led/broker/service/MqttService.java
@@ -17,7 +17,7 @@ public class MqttService {
         message = message.replaceAll("#", "");
         Message<String> mqttMessage = MessageBuilder.withPayload(message)
                 .setHeader(MqttHeaders.TOPIC, topic)
-                .setHeader(MqttHeaders.RETAINED, true)
+                .setHeader(MqttHeaders.RETAINED, reter)
                 .build();
 
         System.out.println("Comando enviado para: " + message);
