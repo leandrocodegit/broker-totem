78e286e883e40e5cd5b9f0babb73d9a3cfcb50f8
leandrocodegit <lpoliveira.ti@gmail.com>
2025-04-23 12:51:05 -0300

    timers schedule

--- src/main/java/com/led/broker/config/ScheduleConfig.java
+++ src/main/java/com/led/broker/config/ScheduleConfig.java
@@ -1,15 +1,23 @@
 package com.led.broker.config;
 
+import com.led.broker.model.Log;
+import com.led.broker.model.constantes.Comando;
 import com.led.broker.model.constantes.Topico;
 import com.led.broker.service.ComandoService;
+import com.led.broker.service.CorService;
 import com.led.broker.service.DispositivoService;
 import com.led.broker.service.MqttService;
+import com.led.broker.util.TimeUtil;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.led.broker.model.constantes.Topico.DASHBOARD;
 
@@ -20,6 +28,7 @@ public class ScheduleConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(ScheduleConfig.class);
     private final MqttService mqttService;
+    private final CorService corService;
 
     @Scheduled(fixedRate = 1 * 60 * 1000)
     public void atualizarDashboardAoSincronizarTodos() {
@@ -31,6 +40,23 @@ public class ScheduleConfig {
                 var clienteId = ComandoService.clientes.remove(cliente.toString());
                 mqttService.sendRetainedMessage(DASHBOARD + "/" + clienteId, "Atualizando dashboard");
             });
+        }
+    }
+
+    @Scheduled(fixedRate = 1 * 60 * 1000)
+    public void checkTimers() {
+        logger.info("Checando timers: " + TimeUtil.timers.size());
+        List<String> devicesRemove = new ArrayList<>();
+        TimeUtil.timers.values().forEach(device -> {
+            if(!TimeUtil.isTime(device)) {
+                corService.salvarCorTemporizada(null, device.getId(), true,true, "Sistema");
+            }
+        });
+        if(!devicesRemove.isEmpty()){
+            devicesRemove.forEach(dev -> {
+                TimeUtil.timers.remove(dev);
+            });
+            devicesRemove.clear();
         }
     }
 }
