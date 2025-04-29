d88b2f4085ac871447075425ebe1abee6197036f
leandrocodegit <lpoliveira.ti@gmail.com>
2025-04-28 21:47:09 -0300

    Habilitando firmware

--- src/main/java/com/led/broker/service/ComandoService.java
+++ src/main/java/com/led/broker/service/ComandoService.java
@@ -173,7 +173,7 @@ public class ComandoService {
         }
         Mono<String> mono = createMono(id);
 
-           mqttService.sendRetainedMessage(Topico.DEVICE_RECEIVE + dispositivo.getId(), host);
+           mqttService.sendRetainedMessage(Topico.DEVICE_RECEIVE + dispositivo.getId(), ComandoFormater.gerarCodigoFirmware(host));
         return mono;
     }
 
