baeb1408606eb5546b864b18ca3528b21f05b79e
leandrocodegit <lpoliveira.ti@gmail.com>
2025-11-24 21:24:17 -0300

    Removito cancel pulls

--- src/main/java/com/led/broker/util/CorUtil.java
+++ src/main/java/com/led/broker/util/CorUtil.java
@@ -110,7 +110,7 @@ public class CorUtil {
             }
         }
 
-        if (Boolean.FALSE.equals(dispositivo.isIgnorarAgenda()) && dispositivo.getOperacao().getModoOperacao().equals(ModoOperacao.AGENDA)) {
+        if (Boolean.FALSE.equals(dispositivo.isIgnorarAgenda()) && (dispositivo.getOperacao().getModoOperacao().equals(ModoOperacao.AGENDA) || dispositivo.getOperacao().getAgenda() != null)) {
             Agenda agenda = dispositivo.getOperacao().getAgenda();
             if (agenda != null && agenda.getCor() != null && agenda.isAtivo() && (agenda.getDispositivos().contains(dispositivo.getId()) || agenda.isTodos())) {
                 if (verificaSeAgendaValida(agenda, dispositivo.getId()))
